b776ad49d4a06c71887547c4f52f62c7f9c3b586
rakshashivanna <shivansa@mail.uc.edu>
2026-08-31 16:54:55 -0400

    Delete Innovus/Run2_OutputFiles directory

--- Innovus/Run2_OutputFiles/Route/xcelium.d/run.d/files.ts
+++
@@ -1,3 +0,0 @@
-1730248730 /home/shivansa/CUSTOM-CIRCUIT-DESIGN/HW4_shivansa/Innovus/Run2_OutputFiles/Route/recognizer_map.v
-1730844410 /home/shivansa/CUSTOM-CIRCUIT-DESIGN/HW4_shivansa/Innovus/Run2_OutputFiles/Route/tb_recognizer.v
-1729784274 /opt/eda/cadence/local/GSCLIB045_Fall2024_SymbolwithVDDVSS/lan/flow/t1u1/reference_libs/GPDK045/gsclib045_all_v4.4/gsclib045/verilog/slow_vdd1v2_basicCells.v
