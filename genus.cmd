56af98c18e4a307529d887992fa48869cbabfb96
shivansa <shivansa@ece-vemurir-9.rhod.uc.edu>
2026-08-31 16:26:11 -0400

    Remove numbered Genus and SimVision artifacts

--- genus.cmd
+++
@@ -1,6 +0,0 @@
-# Cadence Genus(TM) Synthesis Solution, Version 21.19-s055_1, built May 29 2024 16:33:07
-
-# Date: Sun Nov 24 18:41:46 2024
-# Host: ece-rh810-8 (x86_64 w/Linux 5.15.0-124-generic) (20cores*28cpus*1physical cpu*Intel(R) Core(TM) i7-14700K 33792KB)
-# OS:   CentOS Linux release 7.9.2009 (Core)
-
